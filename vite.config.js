7684d2237b323fd375d5680db0f5d1822af5c03d
Savelev Andrey <needway@list.ru>
2026-04-20 02:13:09 +0300

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/mainproject_of_my_life/", // ← ВАЖНО
   assetsInclude: ["**/*.PNG", "**/*.JPG", "**/*.JPEG", "**/*.MP4", "**/*.MP3"],
 });
